52f097d048637e7a9aef576264b713c1bd75d877
maccyber <jonas.enge@gmail.com>
2019-10-28 15:53:50 +0100

    hands

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Metadatagenerator API
+
+### Example
+
+```
+npm run dev
+curl -H "Accept: application/json" http://localhost:3000/api/document/overforingsbrev --data '{ "manager": { "name": "Ole Wiik" }, "employee": { "id": "26118600000", "name": "Maccyber" } }'
+```
--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const router = Router()
 router.use(cors())
 
 // Routes
-router.get('/', handler.getFrontpage)
-router.get('/api/case/:id', handler.getCaseMetadata)
-router.get('/api/document/', handler.getDocumentTypes)
-router.get('/api/document/:id', handler.getDocumentMetadata)
+// router.get('/', handler.getFrontpage)
+// router.get('/api/case/:id', handler.getCaseMetadata)
+// router.get('/api/document/', handler.getDocumentTypes)
+router.post('/api/document/:id', handler.getDocumentMetadata)
 
-router.get('/api/contactperson', handler.getContactPersonMetadata)
-router.get('/api/privateperson', handler.getPrivatePersonMetadata)
-router.get('/api/enterprise', handler.getEnterpriseMetadata)
+// router.get('/api/contactperson', handler.getContactPersonMetadata)
+// router.get('/api/privateperson', handler.getPrivatePersonMetadata)
+// router.get('/api/enterprise', handler.getEnterpriseMetadata)
 
 module.exports = (request, response) => {
   router(request, response, finalhandler(request, response))
