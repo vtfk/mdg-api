5aefd94c17d5c2b58c20ee7e99bcd6fe242740c2
Mats Andreassen <matsand@live.no>
2019-10-28 14:48:03 +0100

    Added routes

---
+++ index.js
@@ -0,0 +1,22 @@
+const Router = require('router')
+const finalhandler = require('finalhandler')
+const cors = require('cors')
+const handler = require('./lib/handler')
+
+// Initialize router
+const router = Router()
+router.use(cors())
+
+// Routes
+router.get('/', handler.getFrontpage)
+router.get('/api/case/:id', handler.getCaseMetadata)
+router.get('/api/document/', handler.getDocumentTypes)
+router.get('/api/document/:id', handler.getDocumentMetadata)
+
+router.get('/api/contactperson', handler.getContactPersonMetadata)
+router.get('/api/privateperson', handler.getPrivatePersonMetadata)
+router.get('/api/enterprise', handler.getEnterpriseMetadata)
+
+module.exports = (request, response) => {
+  router(request, response, finalhandler(request, response))
+}
